fd5ded8e0c71e60eb06c268ab4e8180a9974c1f3
taha <taha_meskini@ieee.org>
2022-07-15 15:21:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      url:'https://yts.torrentbay.to/api/v2/list_movies.json',
+      movies:[],
+      isLoading: true
+    }
+  }
+  componentDidMount(){
+    this.getMoviesList();
+  }
+  getMoviesList(){
+    fetch(this.state.url,)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            movies: result.data.movies,
+            isLoading : false
+          })
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoading: false,
+            error
+          });
+        }
+      )
+  }
+
+
+
+
+
+
+  render(){
+    return(
+      <div className="container my-5">
+        {
+          this.state.isLoading === true ?
+          <div className="text-center">
+              loading ...
+          </div> :
+          <div className="row">
+            {
+              this.state.movies.map((movie) => {
+                return(
+                  <div className="col-sm-4">
+                    <div className="card">
+                      <div className="card-body">
+                        <h6>{movie.title}</h6>
+                        <img src={movie.background_image_original} width='100%' />
+                      </div>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+        }
+      </div>
+    )
+  }
+
+
 }
-
 export default App;
